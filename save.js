f158f7ca0f1f8fa7515715362f7a5a1cbf565ccc
HiGarfield <HiGarfield@126.com>
2021-09-05 14:37:19 +0800

    save cache only when cache does not hit

--- save.js
+++ save.js
@@ -1,40 +1,43 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const execSync = require('child_process').execSync;
+const core = require("@actions/core");
+const execSync = require("child_process").execSync;
 
 try {
-  var paths = new Array();
-  var mixkey = core.getInput('mixkey');
-  var keyString = mixkey ? mixkey + "-cache-openwrt" : "cache-openwrt";
-  const prefix = core.getInput('prefix');
-  if ( prefix != '' ){
-    process.chdir(prefix);
-  }
+    if (core.getState("CACHE_STATE") != "hit") {
+        var paths = new Array();
+        var mixkey = core.getInput("mixkey");
+        var keyString = mixkey ? mixkey + "-cache-openwrt" : "cache-openwrt";
+        const prefix = core.getInput("prefix");
+        if (prefix != "") {
+            process.chdir(prefix);
+        }
 
-  const toolchain = core.getInput('toolchain');
-  if ( toolchain=='true' ){
-    stdout=execSync('git log --pretty=tformat:"%h" -n1 tools toolchain').toString().trim();
-    keyString=keyString+'-'+stdout;
-    paths.push('staging_dir/host*');
-    paths.push('staging_dir/tool*');
-  }
+        const toolchain = core.getInput("toolchain");
+        if (toolchain == "true") {
+            stdout = execSync(
+                'git log --pretty=tformat:"%h" -n1 tools toolchain'
+            )
+                .toString()
+                .trim();
+            keyString = keyString + "-" + stdout;
+            paths.push("staging_dir/host*");
+            paths.push("staging_dir/tool*");
+        }
 
-  const ccache = core.getInput('ccache');
-  if ( ccache=='true' ){
-    stdout=execSync('date +%s').toString().trim();
-    keyString=keyString+'-'+stdout;
-    paths.push('.ccache');
-  }
+        const ccache = core.getInput("ccache");
+        if (ccache == "true") {
+            stdout = execSync("date +%s").toString().trim();
+            keyString = keyString + "-" + stdout;
+            paths.push(".ccache");
+        }
 
-  const cache = require('@actions/cache');
-  console.log(keyString);
-  const cacheId = cache.saveCache(paths, keyString)
-  .then(res =>{
-    if ( typeof res !== 'undefined' && res ){
-      console.log(res, ' cache saved');
+        const cache = require("@actions/cache");
+        console.log(keyString);
+        const cacheId = cache.saveCache(paths, keyString).then((res) => {
+            if (typeof res !== "undefined" && res) {
+                console.log(res, " cache saved");
+            }
+        });
     }
-  })
-
 } catch (error) {
-  core.warning(error.message);
+    core.warning(error.message);
 }
