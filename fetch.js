0a0bb1e7d741c566cc7f4a5b220aca99f17ad9b5
w311ang <30381083+w311ang@users.noreply.github.com>
2022-07-28 13:14:11 +0800

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -4,7 +4,7 @@ const execSync = require('child_process').execSync;
 
 try {
   var paths = new Array();
-  branch = process.env.GITHUB_REF_NAME
+  const branch = process.env.GITHUB_REF_NAME
   var keyString = 'cache-openwrt'+'-'+branch;
   var restoreKeys = new Array();
   const prefix = core.getInput('prefix');
